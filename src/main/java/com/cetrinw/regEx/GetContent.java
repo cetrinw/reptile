b52e834da892e00605e2d1294105a0fd5b5622c7
cetrinw <cetrinw@sina.com>
2016-03-31 16:50:06 +0800

    Rejected commit: Default

---
+++ src/main/java/com/cetrinw/regEx/GetContent.java
@@ -0,0 +1,22 @@
+package com.cetrinw.regEx;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Cetrin Wang on 2016/3/31.
+ * 获得正文
+ */
+public class GetContent {
+    private static final String regEx = "<p>(.+)</p>";
+
+    public static String getContent(String str){
+        Pattern p = Pattern.compile(regEx);
+        Matcher m = p.matcher(str);
+
+        while(m.find()){
+            return m.group();
+        }
+        return null;
+    }
+}
